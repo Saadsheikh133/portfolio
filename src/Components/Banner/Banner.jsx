f364c7d13e4580a28f1bf517e8a271550df001cc
saadsheikh133 <mdsaadsheikh7096@gmail.com>
2023-07-04 18:04:43 +0600

    added projects slider images

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,28 +3,35 @@ import "@lottiefiles/lottie-player";
 import resume from '../../assets/Front-end developer resume of Saad Bin Abdus Salam.pdf'
 import { FaDownload } from "react-icons/fa";
 import { Link } from "react-scroll";
+import { motion } from "framer-motion"
 
 const Banner = () => {
 
 
     return (
-        <div id="/" className="mt-32 py-10 bg-white text-black lg:px-2 md:flex justify-between items-center">
-            <div>
-                <h2 className="text-4xl font-bold">Hi, I am <span className="text-orange-400">Md: Saad Bin Abdus Salam.</span></h2>
-                <Text></Text>
+        <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }} 
+        >
+            <div id="/" className="mt-32 py-10 bg-white text-black lg:px-2 md:flex justify-between items-center">
                 <div>
-                    <Link to="contact" className="btn btn-primary mr-4">Hire Me</Link>
-                    <a href={resume} download className="btn btn-primary"><FaDownload></FaDownload> Resume</a>
+                    <h2 className="text-4xl font-bold">Hi, I am <span className="text-orange-400">Md: Saad Bin Abdus Salam.</span></h2>
+                    <Text></Text>
+                    <div>
+                        <Link to="contact" className="btn btn-primary mr-4">Hire Me</Link>
+                        <a href={resume} download className="btn btn-primary"><FaDownload></FaDownload> Resume</a>
+                    </div>
                 </div>
+                <lottie-player
+                    autoplay
+                    loop
+                    mode="normal"
+                    src='../../../public/91382-web-development.json'
+                    style={{ width: "400px" }}
+                ></lottie-player>
             </div>
-            <lottie-player
-                autoplay
-                loop
-                mode="normal"
-                src='../../../public/91382-web-development.json'
-                style={{ width: "400px" }}
-            ></lottie-player>
-        </div>
+        </motion.div>
     );
 };
 
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,24 +1,83 @@
 import { FaGithub } from "react-icons/fa";
-// import sports from '../../assets/image/sports.png'
-import Marquee from "react-fast-marquee";
-import banner from '../../assets/image/banner.png'
-import classes from '../../assets/image/class.png'
-import instructor from '../../assets/image/instructor.png'
-import footer from '../../assets/image/footer.png'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 
+import banner from '../../assets/image/banner.png'
+import classes from '../../assets/image/classes.png'
+import classes2 from '../../assets/image/class.png'
+import instructor from '../../assets/image/instructor.png'
+import instructors from '../../assets/image/instructors.png'
+import footer from '../../assets/image/footer.png'
+import manageClass from '../../assets/image/manageClass.png'
+import manageUser from '../../assets/image/manageUser.png'
+
+import toyB from '../../assets/image/toyB.png'
+import toy from '../../assets/image/toy.png'
+import newToy from '../../assets/image/newToy.png'
+import best from '../../assets/image/best.png'
+import toyFooter from '../../assets/image/toyFooter.png'
+import category from '../../assets/image/category.png'
+import allToys from '../../assets/image/allToys.png'
+import addToy from '../../assets/image/addToy.png'
+
+import foodB from '../../assets/image/foodB.png'
+import chef from '../../assets/image/chef.png'
+import chefs from '../../assets/image/chefs.png'
+import pitha from '../../assets/image/pitha.png'
+import slider from '../../assets/image/slider.png'
+import foodfoter from '../../assets/image/foodfoter.png'
+import blog from '../../assets/image/blog.png'
 
 const Projects = () => {
     return (
         <div id="project" className="my-10">
             <h2 className="text-5xl text-center font-bold my-10"><span className="text-orange-500">My</span> Projects</h2>
             <div className="lg:w-full lg:flex gap-4 px-2 my-10 items-center">
-                <div className="lg:w-1/2 rounded-xl border">
-                    <Marquee direction="up" className="w-full overflow-hidden object-none" autoFill={true}>
-                        {/* <img className="rounded-xl mb-4" src={banner} alt="" /> */}
-                        <img className="rounded-xl mb-4 w-full object-cover" src={classes} alt="" />
-                        <img className="rounded-xl mb-4 w-full object-cover" src={instructor} alt="" />
-                        <img className="rounded-xl mb-4 w-full object-cover" src={footer} alt="" />
-                    </Marquee>
+                <div className="lg:w-1/2 rounded-xl mt-10">
+                        <Swiper
+                            effect={'coverflow'}
+                            grabCursor={true}
+                            centeredSlides={true}
+                            slidesPerView={'auto'}
+                            coverflowEffect={{
+                                rotate: 50,
+                                stretch: 0,
+                                depth: 100,
+                                modifier: 1,
+                                slideShadows: true,
+                            }}
+                            pagination={true}
+                            modules={[EffectCoverflow, Pagination]}
+                            className="mySwiper"
+                        >
+                            <SwiperSlide>
+                                <img className="lg:h-[500px] rounded-xl" src={banner} />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={classes} />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={classes2} />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={instructor} />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={instructors} />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={footer} />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={manageClass} />
+                            </SwiperSlide>
+                            <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={manageUser} />
+                            </SwiperSlide>
+                        </Swiper>
                 </div>
                 <div className="lg:w-1/2 mt-10 bg-gradient-to-r from-white to-cyan-100 text-black px-4 py-4 shadow-md shadow-white rounded-xl">
                     <h2 className="uppercase text-3xl font-semibold text-center pb-4">sports today</h2>
@@ -39,9 +98,49 @@ const Projects = () => {
                     </div>
                 </div>
             </div>
-            <div className="lg:w-full lg:flex px-2 my-20">
-                <div className="lg:w-1/2">
-                    <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora ipsa maiores aliquid blanditiis possimus numquam enim sapiente in animi, explicabo ullam consequatur veritatis magnam modi saepe delectus laborum? Reprehenderit, alias.</h2>
+            <div className="lg:w-full lg:flex gap-4 px-2 my-20">
+                <div className="lg:w-1/2 rounded-xl mt-10">
+                    <Swiper
+                        effect={'coverflow'}
+                        grabCursor={true}
+                        centeredSlides={true}
+                        slidesPerView={'auto'}
+                        coverflowEffect={{
+                            rotate: 50,
+                            stretch: 0,
+                            depth: 100,
+                            modifier: 1,
+                            slideShadows: true,
+                        }}
+                        pagination={true}
+                        modules={[EffectCoverflow, Pagination]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={toyB} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={toy} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={newToy} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={best} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={category} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={toyFooter} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={allToys} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={addToy} />
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
                 <div className="lg:w-1/2 bg-gradient-to-r mt-10 from-white to-cyan-100 text-black px-4 py-4 shadow-md shadow-white rounded-xl">
                     <h2 className="uppercase text-3xl font-semibold text-center pb-4">Toy Collections</h2>
@@ -62,12 +161,49 @@ const Projects = () => {
                     </div>
                 </div>
             </div>
-            <div className="lg:w-full lg:flex px-2 my-20">
-                <div className="lg:w-1/2">
-                    <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora ipsa maiores aliquid blanditiis possimus numquam enim sapiente in animi, explicabo ullam consequatur veritatis magnam modi saepe delectus laborum? Reprehenderit, alias.</h2>
+            <div className="lg:w-full lg:flex gap-4 px-2 my-20">
+                <div className="lg:w-1/2 rounded-xl mt-10">
+                    <Swiper
+                        effect={'coverflow'}
+                        grabCursor={true}
+                        centeredSlides={true}
+                        slidesPerView={'auto'}
+                        coverflowEffect={{
+                            rotate: 50,
+                            stretch: 0,
+                            depth: 100,
+                            modifier: 1,
+                            slideShadows: true,
+                        }}
+                        pagination={true}
+                        modules={[EffectCoverflow, Pagination]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={foodB} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={chef} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={chefs} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={pitha} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={slider} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={foodfoter} />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img className="lg:h-[500px] rounded-xl" src={blog} />
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
                 <div className="lg:w-1/2 mt-10 bg-gradient-to-r from-white to-cyan-100 text-black px-4 py-4 shadow-md shadow-white rounded-xl">
-                    <h2 className="uppercase text-3xl font-semibold text-center pb-4">Toy Collections</h2>
+                    <h2 className="uppercase text-3xl font-semibold text-center pb-4">Healthy Foods</h2>
                     <div className="lg:text-center text-start my-4">
                         <a className="text-center text-xl text-blue-600 hover:underline border border-blue-500 px-4 py-2 rounded-2xl mr-4 hover:bg-gradient-to-r from-cyan-500 to-purple-500 hover:text-white" href="https://chef-recipe-hunter-483fd.firebaseapp.com/">Live Link</a>
                         <a className="text-center text-xl text-blue-600 hover:underline border border-blue-500 px-4 py-2 rounded-2xl mr-4 hover:bg-gradient-to-r from-cyan-500 to-purple-500 hover:text-white" href="https://github.com/Saadsheikh133/chef-recipe-client"><FaGithub className="inline lg:mr-2 mr-1"></FaGithub>Client</a>
